e47d06e1d4e7254501a44878cd4c3bd793cd3dac
rajabboyDeveloper <99048300+NaimovRajabboy@users.noreply.github.com>
2023-07-06 17:38:36 +0500

    first add

---
+++ main.js
@@ -0,0 +1,39 @@
+// + - / % * < > <=  >= == === != !== i++  a=a+7
+
+// sahrtli operatorlar
+
+// if else
+// if (sharti ) {
+//     bajariladigan kodi
+//   } else {
+//     bajariladigan kodi
+
+//   }
+
+// if (77 > 9) {
+//   console.log("77 katta ");
+// } else {
+//   console.log("9 katta");
+// }
+
+// alert prompt confirm
+// && - va
+// || - yoki
+// ! - emas
+// alert("61161");
+let son = prompt(" 100  сонни киритинг ?");
+if (son >= 100) {
+  console.log("son 100kata yoki teng");
+} else {
+  console.log("100 kichik");
+}
+
+// if (5 > 7) {
+//   console.log("birinchi shart tugri ");
+// } else if (5 > 8) {
+//   console.log("ikinchi shart tugri");
+// } else if (5 > 1) {
+//   console.log("3 shart tugri ");
+// }else{
+//     console.log("bironta shart tugri emas");
+// }
